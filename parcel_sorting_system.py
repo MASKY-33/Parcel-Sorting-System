4d038d7dcf3400e2bbe76eb3a65ed4390292b8e5
MASKY-33 <masky.somebody33@gmail.com>
2026-06-28 16:59:34 +0200

    Update parcel_sorting_system.py

--- parcel_sorting_system.py
+++ parcel_sorting_system.py
@@ -1,4 +1,4 @@
-# P.S.) Building this Project-System really took me mentally deep for a whole afternoon until the next morning to get it fully working.
+# P.S.) Building this Project-System took me mentally deep for a whole afternoon until the next morning to get it fully working.
 # It was so terrible that it was amazing!
 # It was purely because I was searching for the correct System-architecture (flows), for making it work correcty.
 
